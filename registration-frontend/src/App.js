ce30f5b38a8036237cd0f0826aadaf4e8cfeabf9
AryaMS2000 <78840262+AryaMS2000@users.noreply.github.com>
2021-07-17 11:56:14 +0530

    Update App.js

--- registration-frontend/src/App.js
+++ registration-frontend/src/App.js
@@ -1,13 +1,23 @@
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Register from './Components/Register';
+import login from './Components/login';
 
 function App() {
   return (
    
-      <Register></Register>
+      //<Register></Register>
+      <Router>
+    <Switch>
+	  <Route exact path='/'  component={Register}/>
+		<Route exact path='/login' component={login} />
+		</Switch>
+    </Router>
+
       );
    
   }
 
 export default App;
+
